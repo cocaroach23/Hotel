b4e84238adf0a4383f3024c4f57502310b51ab57
cocaroach23 <cocaroach23@gmail.com>
2023-07-07 11:03:46 +0300

    обновление 07.07

--- src/app.js
+++ src/app.js
@@ -1,5 +1,35 @@
+const { Container } = require("postcss")
+
 async function appealServ() { 
     const response = await fetch( "https://www.bit-by-bit.ru/api/student-projects/tours")
     const tours = await response.json()
     console.log(tours)
 }
+
+function renderTours() {
+    Container.innerHTML=""
+        tours.foreach(function(tour){
+            Container.innerHTML +=`
+                <div class="card_hotel">
+                    <div>
+                        <p class="city">${tour.city}</p>
+                        <p class="country">${tour.countru}</p>
+                        <p class="end_time">${tour.endTime}</p>
+                        <h1 class="name_hotel">${tour.hotelName}</h1>
+                    </div>
+                    <img clas="image" src="${tour.image} alt="">
+                    <div>
+                        <p class="price">${trour.price}</p>
+                        <p class="rating">${tour.rating}</p>
+                        <p class="start_time">${tour.startTime}</p>
+                    </div>
+            `
+        })   
+appealServ()
+
+}
+
+
+
+renderTours()
+appealServ()
