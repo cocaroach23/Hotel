40cf68947c66dd39942da4c43188ba29f9eabea7
cocaroach23 <cocaroach23@gmail.com>
2023-08-22 13:38:42 +0300

    вопрос по async

--- src/app.js
+++ src/app.js
@@ -1,35 +1,68 @@
-const { Container } = require("postcss")
+const container = document.getElementById("container")
 
 async function appealServ() { 
     const response = await fetch( "https://www.bit-by-bit.ru/api/student-projects/tours")
     const tours = await response.json()
+
+    renderTours(tours)
     console.log(tours)
 }
 
-function renderTours() {
-    Container.innerHTML=""
-        tours.foreach(function(tour){
-            Container.innerHTML +=`
+function renderTours(tours) {
+    container.innerHTML=""
+        tours.forEach(function(tour){
+            container.innerHTML +=`
                 <div class="card_hotel">
                     <div>
                         <p class="city">${tour.city}</p>
-                        <p class="country">${tour.countru}</p>
+                        <p class="country">${tour.country}</p>
                         <p class="end_time">${tour.endTime}</p>
                         <h1 class="name_hotel">${tour.hotelName}</h1>
                     </div>
                     <img clas="image" src="${tour.image} alt="">
                     <div>
-                        <p class="price">${trour.price}</p>
+                        <p class="price">${tour.price}</p>
                         <p class="rating">${tour.rating}</p>
                         <p class="start_time">${tour.startTime}</p>
                     </div>
             `
         })   
-appealServ()
-
 }
 
+function filterByCountry(tours, country) {
+    if (country){
+        const filteredTours = tours.filter((tour) => {
+            return tour.country === country
+    })
+        renderTours (filteredTours)
+    } else {
+        renderTours(tours)
+}
+}
 
+function filterByCity(tours, rating) {
+    if (rating){
+        const filtredRating = tours.filter((tour) => {
+            return tour.rating === rating
+        })
+        renderTours(filtredRating)
+    } else {
+        renderTours(tours)
+    }
+}
 
-renderTours()
+async function init() {         //
+    const tours = await loadTours()
+    renderTours(tours)
+
+    document.getElementById('indonesia').addEventListener('click', () => filterByCountry(tours, 'Индонезия'))
+    document.getElementById('thailand').addEventListener('click', () => filterByCountry(tours, 'Тайланд'))
+    document.getElementById('maldives').addEventListener('click', () => filterByCountry(tours, 'Мальдивы'))
+    document.getElementById('all').addEventListener('click', () => filterByCountry(tours))
+
+    document.getElementById('five_stars').addEventListener('click', () => filterByCity(tours, '5 звёзд'))
+}
+
+init()
 appealServ()
+// сортировки по фильтрам 
